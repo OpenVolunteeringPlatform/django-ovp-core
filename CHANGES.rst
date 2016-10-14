a63514d60e6ba121e9e911f58f706b5d30e25e59
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-14 20:23:29 -0300

    Release 0.1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -7,8 +7,11 @@ v0.1.0
 * Start project
 * Update README
 
-v0.1.1[unreleased]
+v0.1.1
 -----------
 * Add codeship and codecov badge
 * Add pypi badge
 * Add OvpSchemaGenerator, which fixes multiple methods on @list_route or @detail_route
+
+v0.1.2[unreleased]
+-----------
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name='ovp-core',
-    version='0.1.0',
+    version='0.1.1',
     author=u'Atados',
     author_email='arroyo@atados.com.br',
     packages=['ovp_core'],
     url='https://github.com/OpenVolunteeringPlatform/django-ovp-core',
-    download_url = 'https://github.com/OpenVolunteeringPlatform/django-ovp-core/tarball/0.1.0',
+    download_url = 'https://github.com/OpenVolunteeringPlatform/django-ovp-core/tarball/0.1.1',
     license='AGPL',
     description='This module has core functionality for' + \
                 ' ovp projects, such as creation, editing' + \
