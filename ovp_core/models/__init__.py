418c5252299fd1504e5a64f69bc07050f8be4a28
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-24 23:06:21 -0200

    Add skill and cause models

--- ovp_core/models/__init__.py
+++ ovp_core/models/__init__.py
@@ -1,1 +1,3 @@
 from ovp_core.models.address import GoogleAddress, AddressComponent, AddressComponentType, GoogleRegion
+from ovp_core.models.causes import Cause
+from ovp_core.models.skills import Skill
