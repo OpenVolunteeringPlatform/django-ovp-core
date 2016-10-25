2bf7a6ee2972c0497b5c194055226d7625385d33
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-10-24 23:24:20 -0200

    Rename skills.py and causes.py

---
+++ ovp_core/models/cause.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Cause(models.Model):
+  name = models.CharField('name', max_length=100)
+
+  def __str__(self):
+    return self.name
+
+  class Meta:
+    app_label = 'ovp_core'
+    verbose_name = 'cause'
+    verbose_name_plural = 'causes'
---
+++ ovp_core/models/skill.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Skill(models.Model):
+  name = models.CharField('name', max_length=100)
+
+  def __str__(self):
+    return self.name
+
+  class Meta:
+    app_label = 'ovp_core'
+    verbose_name = 'skill'
