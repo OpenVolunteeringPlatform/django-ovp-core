511aac40dd13aa71a14397ef5804cb44dbca0b64
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-11-09 14:24:28 -0200

    Add tests for Skills and Causes

--- ovp_core/tests/test_models.py
+++ ovp_core/tests/test_models.py
@@ -3,6 +3,8 @@ from django.test import TestCase
 from ovp_core.models import GoogleAddress
 from ovp_core.models import AddressComponent
 from ovp_core.models import AddressComponentType
+from ovp_core.models import Skill
+from ovp_core.models import Cause
 
 class GoogleAddressModelTestCase(TestCase):
   def test_api_call(self):
@@ -36,3 +38,57 @@ class AddressComponentModelTestCase(TestCase):
     a.save()
 
     self.assertTrue(a.__str__() == "long")
+
+class SkillModelTestCase(TestCase):
+  def test_name_max_length(self):
+    """ Assert skill name can't be longer than 100 """
+    n = "a" * 100
+    s = Skill(name=n)
+    s.full_clean()
+    s.save()
+
+    self.assertTrue(s.id > 0)
+
+    n = ("a" * 101)
+    s = Skill(name=n)
+
+    with self.assertRaises(Exception) as context:
+      s.full_clean()
+
+    self.assertTrue('Ensure this value has at most 100 characters' in str(context.exception))
+
+
+  def test_str_method_returns_name(self):
+    """ Assert skill __str__ method returns name """
+    n = "a" * 100
+    s = Skill(name=n)
+    s.save()
+
+    self.assertTrue(s.__str__() == n)
+
+class CauseModelTestCase(TestCase):
+  def test_name_max_length(self):
+    """ Assert cause name can't be longer than 100 """
+    n = "a" * 100
+    c = Cause(name=n)
+    c.full_clean()
+    c.save()
+
+    self.assertTrue(c.id > 0)
+
+    n = ("a" * 101)
+    c = Cause(name=n)
+
+    with self.assertRaises(Exception) as context:
+      c.full_clean()
+
+    self.assertTrue('Ensure this value has at most 100 characters' in str(context.exception))
+
+
+  def test_str_method_returns_name(self):
+    """ Assert cause __str__ method returns name """
+    n = "a" * 100
+    c = Cause(name=n)
+    c.save()
+
+    self.assertTrue(c.__str__() == n)
