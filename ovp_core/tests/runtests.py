17b8fc4659fb1f5dc85d48f0219609b6b5aaeda2
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-11-09 16:47:58 -0200

    Remove rest_framework_jwt from runtests.py

--- ovp_core/tests/runtests.py
+++ ovp_core/tests/runtests.py
@@ -42,7 +42,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     )
 }
 
