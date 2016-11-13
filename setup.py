6b6af2d6382f26d8aac4efa2c21c193f18874f7b
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-11-13 21:21:31 -0200

    Upgrade drf to 3.5.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     zip_safe=False,
     install_requires = [
       'Django>=1.10.1,<1.11.0',
-      'djangorestframework>=3.4.7,<3.5.0',
+      'djangorestframework>=3.5.3,<3.6.0',
       'codecov>=2.0.5,<2.1.0',
       'coverage>=4.2,<4.3.0'
     ]
