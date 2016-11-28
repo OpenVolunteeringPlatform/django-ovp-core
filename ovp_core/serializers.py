cc3f268c7671eb31926db37adc46a617d3cedaa7
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-11-28 15:17:31 -0200

    Add GoogleAddressCityStateSerializer

--- ovp_core/serializers.py
+++ ovp_core/serializers.py
@@ -2,11 +2,17 @@ from ovp_core import models
 
 from rest_framework import serializers
 
+
 class GoogleAddressSerializer(serializers.ModelSerializer):
   class Meta:
     model = models.GoogleAddress
     fields = ['typed_address', 'typed_address2', 'address_line', 'city_state']
     read_only_fields = ['address_line', 'city_state']
+
+class GoogleAddressCityStateSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = models.GoogleAddress
+    fields = ['city_state']
 
 class SkillSerializer(serializers.ModelSerializer):
   class Meta:
