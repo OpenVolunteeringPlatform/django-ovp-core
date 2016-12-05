4f5cbf5529b8522147e71b970a1fbfea71049388
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-12-05 20:25:50 -0200

    Release 1.0.11

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='ovp-core',
-    version='1.0.10',
+    version='1.0.11',
     author=u'Atados',
     author_email='arroyo@atados.com.br',
     packages=find_packages(),
     include_package_data=True,
     url='https://github.com/OpenVolunteeringPlatform/django-ovp-core',
-    download_url = 'https://github.com/OpenVolunteeringPlatform/django-ovp-core/tarball/1.0.10',
+    download_url = 'https://github.com/OpenVolunteeringPlatform/django-ovp-core/tarball/1.0.11',
     license='AGPL',
     description='This module has core functionality for' + \
                 ' ovp projects, such as creation, editing' + \
