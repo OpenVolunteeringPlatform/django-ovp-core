5429d81c59444b5d35cda38c4584cae3d75952db
Cidiomar Jr <cidiomar.jr@gmail.com>
2017-05-30 17:19:59 -0300

    fix AvailabilitySerializer

--- ovp_core/serializers/availability.py
+++ ovp_core/serializers/availability.py
@@ -4,10 +4,15 @@ from rest_framework import serializers
 
 
 class AvailabilitySerializer(serializers.ModelSerializer):
+  def to_representation(self, instance):
+    return { 'weekday': instance.weekday, 'period': instance.period }
+
   def validate(self, data):
-    weekday, period = data.pop('weekday', 0), data.pop('period', 0)
-    return {  'period_index': Availability.compose_period_index_for(weekday, period)  }
+    if not 'period_index' in data:
+      data['period_index'] = Availability.compose_period_index_for(data.get('weekday', 0), data.get('period', 0))
+    return super().validate(data)
 
   class Meta:
     fields = ['weekday', 'period']
     model = Availability
+
