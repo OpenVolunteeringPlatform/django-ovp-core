1ca0fcd1f2a657e3cb6c11b160d30de145ad5a41
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-06-20 10:42:57 -0300

    Add SimpleAddressSerializer

--- ovp_core/serializers/__init__.py
+++ ovp_core/serializers/__init__.py
@@ -1,5 +1,6 @@
 from .empty import *
 from .google_address import *
+from .simple_address import *
 from .cause import *
 from .skill import *
 from .lead import *
---
+++ ovp_core/serializers/simple_address.py
@@ -0,0 +1,7 @@
+from ovp_core import models
+from rest_framework import serializers
+
+class SimpleAddressSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = models.SimpleAddress
+    fields = ['street', 'number', 'neighbourhood', 'city', 'state', 'zipcode', 'country']
